6c70b3c1b7d059484bfdcb278842f3a17f337964
rfinho <romans.filimonovs.other@gmail.com>
2019-05-24 11:20:11 +0100

    added user input to calculate marks

---
+++ add_marks_total_input.py
@@ -0,0 +1,11 @@
+physics = int(input("Enter Physics mark: "))
+chemistry = int(input("Enter Chemistry mark: "))
+math = int(input("Enter Math mark: "))
+
+
+total = physics + chemistry + math
+per = total * 100 / 450
+print("- - - - - - - - - - - - - !")
+print("Total marks are: ", total)
+print("Percentage is ", per)
+print("- - - - - - - - - - - - - !")
