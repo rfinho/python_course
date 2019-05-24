1414de34e3fb155560e7c804d74b6ced7b207e8f
rfinho <romans.filimonovs.other@gmail.com>
2019-05-24 10:09:03 +0100

    python, first functions

---
+++ add_marks.py
@@ -0,0 +1,6 @@
+physics = 80
+chemistry = 90
+math = 100
+
+total = physics + chemistry + math
+print("the result is: ", total)
---
+++ add_marks_total.py
@@ -0,0 +1,13 @@
+physics = 80
+chemistry = 90
+math = 100
+
+total = physics + chemistry + math
+per = total * 100 / 450
+print("Physics: ", physics)
+print("Chemistry: ", chemistry)
+print("Maths: ", math)
+print("- - - - - - - - - - - - - ")
+print("Total marks are: ", total)
+print("Percentage is ", per)
+print("- - - - - - - - - - - - - ")
