c43af41eb4cc49d3ff03bb7cd2aee4db81c0e0d5
rfinho <romans.filimonovs.other@gmail.com>
2019-05-28 10:53:33 +0100

    added formatting

--- while_loop_with_input_and_limit.py
+++ while_loop_with_input_and_limit.py
@@ -1,7 +1,8 @@
 number = int(input("Enter your number: "))
 limit = int(input("Enter your limit "))
 a = 1
-
+print("====== Multiplication table ======")
 while a <= limit:
     print(number, "x", a, "=", (number * a) )
     a += 1
+print("===================================")
