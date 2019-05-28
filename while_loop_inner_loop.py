37188398b948c4064d73588772a0c1aad03676e6
rfinho <romans.filimonovs.other@gmail.com>
2019-05-28 11:34:13 +0100

    few while nested loops

---
+++ while_loop_inner_loop.py
@@ -0,0 +1,8 @@
+a = 1
+
+while a <= 10:
+    b = 10
+    while b <= 10:
+        print(b)
+        b += 1
+    a += 1
---
+++ while_loop_nested.py
@@ -0,0 +1,8 @@
+a = 1
+
+while a <= 10:
+    b = 1
+    while b <= a:
+        print(a)
+        b += 1
+    a += 1
