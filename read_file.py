55bb8f6c4fd47d02d60e297550513bcd948c7782
rfinho <romans.filimonovs.other@gmail.com>
2019-05-30 16:49:22 +0100

    read and replace text files function

---
+++ read_file.py
@@ -0,0 +1,17 @@
+f_read = open("data.txt", "r")
+f_write = open("data2.txt", "w")
+find = input("What are you looking for?: ")
+replace = input("replace with: ")
+
+for data in f_read:
+    i = 0
+    while i < len(data):
+        if data[i] == find[0]:
+            if data[i:len(find) + i] == find:
+                print(replace, end="", file=f_write)
+                i += len(find) - 1
+            else:
+                print(data[i], end="", file=f_write)
+        else:
+            print(data[i], end="", file=f_write)
+        i += 1
